21748109c23e7f589a0ab90360cc67f9c3198c28
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-17 19:39:20 +0200

    added gdm and noctalia to the niri setup

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
     programs.niri.enable = true;
+    services.displayManager.gdm.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        fuzzel
+        noctalia-shell
+    ];
 }
