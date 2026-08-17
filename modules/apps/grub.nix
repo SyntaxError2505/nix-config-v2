95feae5480ac718b48ee1678d5b977314eeab4ff
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-17 10:39:38 +0200

    moved grub to module

---
+++ modules/apps/grub.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+}
--- modules/base.nix
+++ modules/base.nix
@@ -1,11 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # latest kernel
