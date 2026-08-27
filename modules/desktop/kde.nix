df22ac871ecdca772f698fde70b6425edc296374
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-27 23:20:37 +0200

    3 commits, one hour of my life, just to come full circle because kde is implemented wierdly on nix

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -5,6 +5,7 @@
         ../services/tailscale.nix
         ../services/xdg.nix
         ../services/networking.nix
+        ../services/bluetooth.nix
     ];
     services.displayManager.plasma-login-manager.enable = true;
     services.desktopManager.plasma6.enable = true;
