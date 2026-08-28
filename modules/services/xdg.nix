da6629a806cb9f8d4579404fc72a3ac31187da99
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-28 12:14:11 +0200

    fixed niri deskop portal

--- modules/services/xdg.nix
+++ modules/services/xdg.nix
@@ -6,7 +6,9 @@
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     config = {
       common.default = [ "gtk" ];
-      niri.default = [ "gnome" "gtk" ];
+      niri = {
+        default = [ "gtk" ];
+      };
       hyprland.default = [ "hyprland" "gtk" ];
       kde.default = [ "kde" "gtk" ];
     };
