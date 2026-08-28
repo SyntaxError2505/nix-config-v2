8a1457fa5e74a9c5c359673c2ba2e341750af831
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-28 11:10:00 +0200

    added kvantum to the kde package

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
     imports = [
@@ -6,4 +6,8 @@
     ];
     services.displayManager.plasma-login-manager.enable = true;
     services.desktopManager.plasma6.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        qt6Packages.qtstyleplugin-kvantum
+    ];
 }
