84a20a1a7809396b3729707d8af0ec5f9fa2ce0d
SyntaxError2505 <mr.sascha.cherevko@gmail.com>
2026-08-28 12:20:09 +0200

    big change for xdg

--- modules/services/xdg.nix
+++ modules/services/xdg.nix
@@ -3,14 +3,34 @@
 {
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-kde
+      xdg-desktop-portal-gtk
+    ];
+
     config = {
-      common.default = [ "gtk" ];
-      niri = {
+      common = {
         default = [ "gtk" ];
       };
-      hyprland.default = [ "hyprland" "gtk" ];
-      kde.default = [ "kde" "gtk" ];
+
+      niri = {
+        default = [ "gnome" "gtk" ];
+      };
+
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+      };
+
+      kde = {
+        default = [ "kde" ];
+      };
+
+      plasma = {
+        default = [ "kde" ];
+      };
     };
   };
 }
